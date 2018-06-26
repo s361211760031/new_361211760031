e349ce646c3a963dbb063effc39abee11e230ac6
s361211760031 <supanee.som40@gmail.com>
2018-06-26 11:53:10 +0700

    Hello

---
+++ src/lab1/myCode.java
@@ -0,0 +1,23 @@
+package lab1;
+
+public class myCode {
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello World.");
+        System.out.println(" น.ส สุภาณี จากถิ่น.");
+        System.out.println(" 361211760031.");
+        System.out.println(" MIT211.");
+
+    }//main
+
+
+
+
+
+
+
+
+
+
+}//class
