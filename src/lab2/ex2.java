f12e4e985995656b083a9196fcb6f8fcab531e26
Supanee <supanee.som40@gmail.com>
2018-07-10 10:05:41 +0700

    input data with System.in.read()

---
+++ src/lab2/ex2.java
@@ -0,0 +1,42 @@
+package lab2;
+
+public class ex2 {
+
+    public static void main(String[] args) {
+        double number;
+        number =15;
+        System.out.println(number);
+
+        char c = 'M';
+        System.out.println(c);
+
+        String msg;
+        msg = "Supanee Jaktin";
+        System.out.println(msg);
+        String msg2 = " MIT ";
+
+        System.out.println(msg+msg2);
+        System.out.println(msg.length());
+        System.out.println(msg.toUpperCase());
+        System.out.println(msg.toLowerCase());
+
+
+        //boolean
+        boolean m = true;
+        System.out.println(m);
+        m = false;
+        System.out.println(m);
+
+
+
+
+
+
+
+    }//main
+
+
+
+
+
+}//class
