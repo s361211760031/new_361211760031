6776f8ed47894ed96f84c35a5e679418ac769fab
Supanee <supanee.som40@gmail.com>
2018-08-28 11:00:44 +0700

    lab4: Methods Overloading

--- src/Quiz1/ex3.java
+++ src/Quiz1/ex3.java
@@ -2,22 +2,6 @@ package Quiz1;
 
 import java.util.Scanner;
 
-public class ex3 {
-    public static void main(String[] args) {
-        int  ;
-        System.out.println("เลือกการค่าห้องพักโรงแรม?");
-        System.out.println("Room Type 1 ");
-        System.out.println("Room Type 2 ");
-        System.out.println("Room Type 3 ");
-        System.out.println("Room Type 4 ");
+public class ex3{}
 
-        Scanner input = new Scanner(System.in);
-        System.out.print("เลือกประเภทสินค้า(1-4):");
-        input.nextInt();
-        //switch-case
 
-        System.out.println("จำนวนวันที่เข้าพัก:");
-
-        System.out.println("ค่าห้องที่ต้องจ่าย:");
-    }
-}
