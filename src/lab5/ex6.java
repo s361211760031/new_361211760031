aaf3400b8746a195e27416bdb92c7046405db9c4
Supanee <supanee.som40@gmail.com>
2018-09-18 11:19:51 +0700

    lab5

---
+++ src/lab5/ex6.java
@@ -0,0 +1,26 @@
+package lab5;
+
+import java.util.StringTokenizer;
+
+public class ex6 {
+    public static void main(String[] args) {
+        StringBuffer myStr = new StringBuffer("Hello") ;
+        System.out.println(myStr);
+        System.out.println(myStr.reverse());
+
+        myStr.append(" MIT211");
+        System.out.println(myStr);
+        myStr.insert(0,"RUTS");
+        System.out.println(myStr);
+
+        //StringBuilder
+        StringBuilder myStr2 = new StringBuilder("Hello");
+        System.out.println(myStr2);
+        myStr2.reverse();
+        System.out.println(myStr2);
+        //StringTokenizer
+        StringTokenizer myStr3 = new StringTokenizer("Hello, my name is som");
+        int count = myStr3.countTokens();
+        System.out.println(count);
+    }
+}
