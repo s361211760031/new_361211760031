094c7a27a8f494b1ad289c0fc1e8ef914524d782
Supanee <supanee.som40@gmail.com>
2018-10-02 11:14:54 +0700

    lab7: Rectangle

---
+++ src/lab7/StudentApp.java
@@ -0,0 +1,18 @@
+package lab7;
+
+public class StudentApp {
+
+    public static void main(String[] args) {
+
+        Student std = new Student("Som","031","supanee.som40@gmail.com","0986754231",21);
+
+
+
+
+
+
+    }
+
+
+
+}
